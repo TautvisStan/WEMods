b74567cca6503d266a6b34486fdd2080e1073e01
Tautvis <tassat12321@gmail.com>
2023-07-19 14:41:57 +0300

    stuff

---
+++ GiantThumbtacks/Plugin.cs
@@ -0,0 +1,58 @@
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.IO;
+
+namespace WrestlingEmpireTemplateMod
+{
+    [BepInPlugin(PluginGuid, PluginName, PluginVer)]
+    [HarmonyPatch]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string PluginGuid = "GeeEm.WrestlingEmpire.GiantThumbtacks";
+        public const string PluginName = "GiantThumbtacks";
+        public const string PluginVer = "1.0.0";
+
+        internal static ManualLogSource Log;
+        internal readonly static Harmony Harmony = new(PluginGuid);
+        public static ConfigEntry<float> configSize;
+        internal static string PluginPath;
+        
+
+        private void Awake()
+        {
+            Plugin.Log = base.Logger;
+
+            PluginPath = Path.GetDirectoryName(Info.Location);
+            configSize = Config.Bind("General",
+             "TacksSize",
+             7.5f,
+             "The size of thumbtacks");
+        }
+
+        private void OnEnable()
+        {
+            Harmony.PatchAll();
+            Logger.LogInfo($"Loaded {PluginName}!");
+        }
+
+        private void OnDisable()
+        {
+            Harmony.UnpatchSelf();
+            Logger.LogInfo($"Unloaded {PluginName}!");
+        }
+
+        [HarmonyPatch(typeof(BHPCHMPHECM), nameof(BHPCHMPHECM.NCANNKNAKBO))]
+        [HarmonyPrefix]
+        public static bool BHPCHMPHECM_NCANNKNAKBO(BHPCHMPHECM __instance, int __0, UnityEngine.Color __1, float __2, UnityEngine.GameObject __3, float __4, float __5, float __6, float __7, float __8, float __9, int __10)
+        {
+            if (__0 == -69 && __2 < configSize.Value)
+            {
+                __instance.NCANNKNAKBO(__0, __1, configSize.Value, __3, __4, __5, __6, __7, __8, __9, __10);
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ GiantThumbtacks/README.md
@@ -0,0 +1,4 @@
+# GiantThumbtacks
+This mod makes bag of thumbtacks spawn giant tacks. The size can be customized in the config file.
+
+Because we put "Death" in "DeathMatch"!
