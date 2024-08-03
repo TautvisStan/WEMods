f571a92b542d47a24913b51bc1384601640d1694
Tautvis <tassat12321@gmail.com>
2024-08-03 16:33:40 +0300

    Tournaments 1.0

--- Tournaments/CustomCard.cs
+++ Tournaments/CustomCard.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Tournaments
 {
@@ -10,50 +11,193 @@ namespace Tournaments
     {
         public List<int> leftWresters { get; set; } = new();
         public List<int> rightWresters { get; set; } = new();
+        public int HigherBracket;
+        public int HigherBracketSide;
+        public void UpdateMatchTitle()
+        {
+            string title = "";
+            switch (id)
+            {
+                case 1:
+                    title = "Finals";
+                    break;
+                case 2:
+                case 3:
+                    title = "Semi Finals";
+                    break;
+                case 4:
+                case 5:
+                case 6:
+                    title = "?????";
+                    break;
+                case 7:
+                case 8:
+                case 9:
+                case 10:
+                    title = "Quarter Finals";
+                    break;
+            }
+            match = title + " " + match;
+        }
+        public void SendFurther(List<int> wrestlers)
+        {
+            if(HigherBracket != -1)
+            {
+                CustomSegment nextSegment = Plugin.TournamentCard.segment[HigherBracket] as CustomSegment;
+                if(HigherBracketSide == 1)
+                {
+                    nextSegment.leftWresters.AddRange(wrestlers);
+                    nextSegment.leftName = GetWinnerName(wrestlers);
+                }
+                else
+                {
+                    nextSegment.rightWresters.AddRange(wrestlers);
+                    nextSegment.rightName = GetWinnerName(wrestlers);
+                }
+                if(nextSegment.leftWresters.Count > 0 && nextSegment.rightWresters.Count > 0)
+                {
+                    nextSegment.time = 0;
+                }
+            }
+            else
+            {
+                if (FFCEGMEAIBP.OOODPHNGHGD != 0)
+                {
+                    Plugin.TournamentCard.Winner = GetWinnerName(wrestlers);
+                }
+                else
+                {
+                    if (FFCEGMEAIBP.OLJFOJOLLOM < 1) //solo
+                    {
+                        Plugin.TournamentCard.Winner = wrestlers.Count + "-way Draw!";
+                    }
+                    else
+                    {
+                        Plugin.TournamentCard.Winner =  "2-way Draw!";
+                    }
+                }
+            }
+        }
+        public static string GetWinnerName(List<int> winner)
+        {
+            if(FFCEGMEAIBP.OLJFOJOLLOM < 1) //solo
+            {
+                if (winner.Count == 1)
+                {
+                    return Characters.c[winner[0]].name;
+                }
+                else
+                {
+                    return "Multiple Wrestlers";
+                }
+            }
+            else   //team
+            {
+                if(winner.Count == 2)
+                {
+                    return Characters.c[winner[0]].BEMDBFHJFAB(Characters.c[winner[1]].id, 1);
+                    /*    if (Characters.c[winner[0]].teamName != "")
+                        {
+                            Debug.LogWarning("TEAM HAS NAME");
+                            return Characters.c[winner[0]].teamName;
+
+                        }
+                        else
+                        {
+                            Debug.LogWarning("TEAM NO NAME");
+                            return Characters.c[winner[0]].name + " & " + Characters.c[winner[1]].name;
+                        }*/
+                }
+                else
+                {
+                    return Characters.c[winner[0]].name + "'s Team";
+                    //return "Multiple Wrestlers";
+                    // return Characters.c[winner[0]].BEMDBFHJFAB(0, 1);
+                }
+
+            }
+        }
     }
     public class CustomCard : Card
     {
         public int CurrentSegment { get; set; }
+        public string Winner { get; set; }
+        public GameObject WinnerObj { get; set; }
         public CustomCard()
         {
+            Winner = "TBD";
             CurrentSegment = 0;
             date = 0;
             size = 10;
             segment = new CustomSegment[this.size + 1];
+            CustomSegment[] customSegment = segment as CustomSegment[];
             for (int i = 1; i <= this.size; i++)
             {
-                segment[i] = new CustomSegment();
-                segment[i].id = i;
+                customSegment[i] = new CustomSegment();
+                customSegment[i].id = i;
 
 
                 switch (i)
                 {
                     case 1:
-                        segment[i].match = "Finals";
-                        segment[i].leftName = "TBD";
-                        segment[i].rightName = "TBD";
-                        segment[i].time = -1;
+                        customSegment[i].match = "Finals";
+                        customSegment[i].leftName = "TBD";
+                        customSegment[i].rightName = "TBD";
+                        customSegment[i].time = -1;
                         break;
                     case 2:
                     case 3:
-                        segment[i].match = "Semi Finals";
-                        segment[i].leftName = "TBD";
-                        segment[i].rightName = "TBD";
-                        segment[i].time = -1;
+                        customSegment[i].match = "Semi Finals";
+                        customSegment[i].leftName = "TBD";
+                        customSegment[i].rightName = "TBD";
+                        customSegment[i].time = -1;
                         break;
                     case 4:
                     case 5:
                     case 6:
-                        segment[i].match = "?????";
-                        segment[i].leftName = "?????";
-                        segment[i].rightName = "?????";
-                        segment[i].time = -1;
+                        customSegment[i].match = "?????";
+                        customSegment[i].leftName = "?????";
+                        customSegment[i].rightName = "?????";
+                        customSegment[i].time = -1;
                         break;
                     case 7:
                     case 8:
                     case 9:
                     case 10:
-                        segment[i].match = "Quarter Finals";
+                        customSegment[i].match = "Quarter Finals";
+                        break;
+                }
+
+                switch (i)
+                {
+                    case 1:
+                        customSegment[i].HigherBracket = -1;
+                        customSegment[i].HigherBracketSide = -1;
+                        break;
+                    case 2:
+                        customSegment[i].HigherBracket = 1;
+                        customSegment[i].HigherBracketSide = 1;
+                        break;
+                    case 3:
+                        customSegment[i].HigherBracket = 1;
+                        customSegment[i].HigherBracketSide = 2;
+                        break;
+
+                    case 7:
+                        customSegment[i].HigherBracket = 2;
+                        customSegment[i].HigherBracketSide = 1;
+                        break;
+                    case 8:
+                        customSegment[i].HigherBracket = 2;
+                        customSegment[i].HigherBracketSide = 2;
+                        break;
+                    case 9:
+                        customSegment[i].HigherBracket = 3;
+                        customSegment[i].HigherBracketSide = 1;
+                        break;
+                    case 10:
+                        customSegment[i].HigherBracket = 3;
+                        customSegment[i].HigherBracketSide = 2;
                         break;
                 }
             }
--- Tournaments/Plugin.cs
+++ Tournaments/Plugin.cs
@@ -1,4 +1,4 @@
-//TODO auto set up the wrestlers; scene nav in the match; ending the match saves the results and sets the winners further
+//TODO  optional bye system when draws; 
 
 using BepInEx;
 using BepInEx.Logging;
@@ -6,7 +6,10 @@ using HarmonyLib;
 using System;
 using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using WECCL.API;
 
 namespace Tournaments
 {
@@ -26,12 +29,20 @@ namespace Tournaments
         static int TournamentButton;
         static readonly int TournamentNum = -789987;
         public static CustomCard TournamentCard { get; set; } = null;
+        public static int OldMenu { get; set; }
 
         private void Awake()
         {
             Plugin.Log = base.Logger;
 
             PluginPath = Path.GetDirectoryName(Info.Location);
+
+
+            Buttons.RegisterCustomButton(this, "Reset the Card", () =>
+            {
+                TournamentCard = null;
+                return "Card reset!";
+            }, true);
         }
 
         private void OnEnable()
@@ -61,8 +72,9 @@ namespace Tournaments
                 if (LIPNHOMGGHF.ODOAPLMOJPD == 1)
                 {
                     LIPNHOMGGHF.DFLLBNMHHIH();
-                    LIPNHOMGGHF.FKANHDIMMBJ[LIPNHOMGGHF.HOAOLPGEBKJ].ICGNAJFLAHL(1, "Tournament", 0f, -0f, 1.5f, 1.5f);
+                    LIPNHOMGGHF.FKANHDIMMBJ[LIPNHOMGGHF.HOAOLPGEBKJ].ICGNAJFLAHL(1, "Tournament", -200f, -175f, 1.5f, 1.5f);
                     TournamentButton = LIPNHOMGGHF.HOAOLPGEBKJ;
+                    LIPNHOMGGHF.DADOHOENFJJ();
                 }
             }
         }
@@ -90,6 +102,7 @@ namespace Tournaments
         [HarmonyPrefix]
         public static void LIPNHOMGGHF_PMIIOCMHEAE_Patch(ref int KBEAJEIMNMI)
         {
+            OldMenu = LIPNHOMGGHF.FAKHAFKOBPB;
             //Doing stuff when going match setup -> game
             if (SceneManager.GetActiveScene().name == "Match_Setup" && NAEEIFNFBBO.CBMHGKFFHJE == TournamentNum && KBEAJEIMNMI == 50)
             {
@@ -102,7 +115,7 @@ namespace Tournaments
             //Doing stuff when exiting match setup
             if (SceneManager.GetActiveScene().name == "Match_Setup" && NAEEIFNFBBO.CBMHGKFFHJE == TournamentNum && KBEAJEIMNMI != 50)
             {
-                if (KBEAJEIMNMI != 11)
+                if (KBEAJEIMNMI != 11)    //char select
                 {
                     NAEEIFNFBBO.CBMHGKFFHJE = TournamentNum;
                     LIPNHOMGGHF.BCKLOCJPIMD = TournamentNum;
@@ -123,24 +136,57 @@ namespace Tournaments
                 return;
             }
 
-            /*     //Doing stuff when exiting match
-                 if (SceneManager.GetActiveScene().name == "Game" && LIPNHOMGGHF.BCKLOCJPIMD == RogueliteNum)
-                 {
-                     if (!Won)
-                     {
-                         NAEEIFNFBBO.CBMHGKFFHJE = RogueliteNum;
-                         LIPNHOMGGHF.BCKLOCJPIMD = RogueliteNum;
-                         KBEAJEIMNMI = 14;  //match setup redirect
-                     }
-                     else
-                     {
-                         Won = false;
-                         NAEEIFNFBBO.CBMHGKFFHJE = 0;
-                         LIPNHOMGGHF.BCKLOCJPIMD = 0;
-                         KBEAJEIMNMI = 1;   //titles
-                     }
-                     return;
-                 }*/
+            //Doing stuff when exiting match
+            if (SceneManager.GetActiveScene().name == "Game" && LIPNHOMGGHF.BCKLOCJPIMD == TournamentNum)
+            {
+                CustomSegment segment = TournamentCard.segment[TournamentCard.CurrentSegment] as CustomSegment;
+                if (FFCEGMEAIBP.LOBDMDPMFLK != 1)
+                {
+                    FFCEGMEAIBP.CPAEBBDOGNN = TournamentCard.CurrentSegment;
+                    FFCEGMEAIBP.PJFOEJPCLFB();
+                    TournamentCard.segment[TournamentCard.CurrentSegment].result = IMNHOCBFGHJ.CIIDDMMENME();
+                    segment.UpdateMatchTitle();
+
+                    List<int> winners = new();
+                    if (FFCEGMEAIBP.OOODPHNGHGD == 0)   //nobody wins, everyone goes further
+                    {
+                        for (int i = 1; i <= NJBJIIIACEP.NBBBLJDBLNM; i++)
+                        {
+                            DFOGOCNBECG dfogocnbecg = NJBJIIIACEP.OAAMGFLINOB[i];
+                            if (dfogocnbecg.FIEMGOLBHIO == 1)
+                            {
+                                winners.Add(dfogocnbecg.GOOKPABIPBC);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (FFCEGMEAIBP.OLJFOJOLLOM < 1)    //solo win
+                        {
+                            winners.Add(NJBJIIIACEP.OAAMGFLINOB[FFCEGMEAIBP.OOODPHNGHGD].GOOKPABIPBC);
+                        }
+                        else        //team win
+                        {
+                            for (int i = 1; i <= NJBJIIIACEP.NBBBLJDBLNM; i++)
+                            {
+                                DFOGOCNBECG dfogocnbecg = NJBJIIIACEP.OAAMGFLINOB[i];
+                                if (dfogocnbecg.FIEMGOLBHIO == 1 && dfogocnbecg.LBCFAJGDKJP == NJBJIIIACEP.OAAMGFLINOB[FFCEGMEAIBP.OOODPHNGHGD].LBCFAJGDKJP)
+                                {
+                                    winners.Add(dfogocnbecg.GOOKPABIPBC);
+                                }
+                            }
+                        }
+                    }
+
+
+                    segment.SendFurther(winners);
+
+                }
+                NAEEIFNFBBO.CBMHGKFFHJE = TournamentNum;
+                LIPNHOMGGHF.BCKLOCJPIMD = TournamentNum;
+                KBEAJEIMNMI = 12;  //card redirect
+                return;
+            }
 
         }
 
@@ -166,15 +212,37 @@ namespace Tournaments
             }
             return true;
         }
-        //Removing the unneeded button
+        //Repositioning stuff in the card screen
         [HarmonyPatch(typeof(Scene_Card), nameof(Scene_Card.Start))]
         [HarmonyPostfix]
         public static void Scene_Card_Start(Scene_Card __instance)
         {
             if (InCustomMode())
             {
-                __instance.gManual.SetActive(false);
+                __instance.gManual.SetActive(false);        //removing unneeded buttons
                 FFCEGMEAIBP.EJBBMLKPNFK[11].SetActive(false);
+                FFCEGMEAIBP.EJBBMLKPNFK[4].SetActive(false);
+                FFCEGMEAIBP.EJBBMLKPNFK[5].SetActive(false);
+                FFCEGMEAIBP.EJBBMLKPNFK[6].SetActive(false);
+
+                FFCEGMEAIBP.EJBBMLKPNFK[1].transform.localPosition = new Vector3(FFCEGMEAIBP.EJBBMLKPNFK[1].transform.localPosition.x, FFCEGMEAIBP.EJBBMLKPNFK[2].transform.localPosition.y, FFCEGMEAIBP.EJBBMLKPNFK[1].transform.localPosition.z);
+
+                FFCEGMEAIBP.EJBBMLKPNFK[2].transform.localPosition = new Vector3(FFCEGMEAIBP.EJBBMLKPNFK[2].transform.localPosition.x, FFCEGMEAIBP.EJBBMLKPNFK[4].transform.localPosition.y, FFCEGMEAIBP.EJBBMLKPNFK[2].transform.localPosition.z);
+                FFCEGMEAIBP.EJBBMLKPNFK[3].transform.localPosition = new Vector3(FFCEGMEAIBP.EJBBMLKPNFK[3].transform.localPosition.x, FFCEGMEAIBP.EJBBMLKPNFK[4].transform.localPosition.y, FFCEGMEAIBP.EJBBMLKPNFK[3].transform.localPosition.z);
+
+
+                FFCEGMEAIBP.EJBBMLKPNFK[4].transform.localPosition = new Vector3(1000f, 0f, 0f);
+                FFCEGMEAIBP.EJBBMLKPNFK[5].transform.localPosition = new Vector3(0f, -1000f, 0f);
+                FFCEGMEAIBP.EJBBMLKPNFK[6].transform.localPosition = new Vector3(-1000f, 0f, 0f);
+
+                LIPNHOMGGHF.DADOHOENFJJ();
+
+
+
+                TournamentCard.WinnerObj = GameObject.Instantiate(__instance.gDate, __instance.gDate.transform.parent);
+                TournamentCard.WinnerObj.transform.localPosition = new Vector3(0f, 220f, 0f);
+                TournamentCard.WinnerObj.GetComponent<Text>().text = "Winner: " + TournamentCard.Winner;
+
             }
         }
         //Redirecting to the match setup
@@ -184,10 +252,38 @@ namespace Tournaments
         {
             if (InCustomMode())
             {
-                if(LIPNHOMGGHF.PIEMLEPEDFN >= 15)
+                if(LIPNHOMGGHF.PIEMLEPEDFN >= 15)  //moving into match setup
                 {
                     TournamentCard.CurrentSegment = LIPNHOMGGHF.NNMDEFLLNBF;
                     LIPNHOMGGHF.PMIIOCMHEAE(14);
+                }
+            }
+        }
+        //Fixing the segment display for ready matches
+        static bool InDescribeSegmentMethod { get; set; } = false;
+        [HarmonyPatch(typeof(Scene_Card), nameof(Scene_Card.DescribeSegment))]
+        [HarmonyPrefix]
+        public static void Scene_Card_DescribeSegmentPRE(Scene_Card __instance, int CPAEBBDOGNN)
+        {
+            if (InCustomMode())
+            {
+                if (TournamentCard.segment[CPAEBBDOGNN].time == 0 && CPAEBBDOGNN <= 3)
+                {
+                    TournamentCard.segment[CPAEBBDOGNN].time = -1;
+                    InDescribeSegmentMethod = true;
+                }
+            }
+        }
+        [HarmonyPatch(typeof(Scene_Card), nameof(Scene_Card.DescribeSegment))]
+        [HarmonyPostfix]
+        public static void Scene_Card_DescribeSegmentPOST(Scene_Card __instance, int CPAEBBDOGNN)
+        {
+            if (InCustomMode())
+            {
+                if (InDescribeSegmentMethod == true)
+                {
+                    TournamentCard.segment[CPAEBBDOGNN].time = 0;
+                    InDescribeSegmentMethod = false;
                 }
             }
         }
@@ -208,29 +304,197 @@ namespace Tournaments
         {
             if (InCustomMode())
             {
-                switch (KJELLNJFNGO)
+                if (TournamentCard.segment[KJELLNJFNGO].leftName == "" || TournamentCard.segment[KJELLNJFNGO].rightName == "")
                 {
-                    case 1:
-                        __result = "Finals";
-                        break;
-                    case 2:
-                    case 3:
-                        __result = "Semi Finals";
-                        break;
-                    case 4:
-                    case 5:
-                    case 6:
-                        __result = "?????";
-                        break;
-                    case 7:
-                    case 8:
-                    case 9:
-                    case 10:
-                        __result = "Quarter Finals";
-                        break;
+                    switch (KJELLNJFNGO)
+                    {
+                        case 1:
+                            __result = "Finals";
+                            break;
+                        case 2:
+                        case 3:
+                            __result = "Semi Finals";
+                            break;
+                        case 4:
+                        case 5:
+                        case 6:
+                            __result = "?????";
+                            break;
+                        case 7:
+                        case 8:
+                        case 9:
+                        case 10:
+                            __result = "Quarter Finals";
+                            break;
+                    }
+                }
+                else
+                {
+                    __result = TournamentCard.segment[KJELLNJFNGO].leftName + " v " + TournamentCard.segment[KJELLNJFNGO].rightName;
                 }
             }
         }
+        //setting up the match
+        [HarmonyPatch(typeof(Scene_Match_Setup), nameof(Scene_Match_Setup.Start))]
+        [HarmonyPostfix]
+        public static void Scene_Match_Setup_Start_Postfix(Scene_Match_Setup __instance)
+        {
+            if (InCustomMode())
+            {
+                if(OldMenu == 12)
+                {
+                    RemoveOpponents();
+                    foreach(int wrestler in ((CustomSegment)TournamentCard.segment[TournamentCard.CurrentSegment]).leftWresters)
+                    {
+                        AddCharacter(wrestler, 1);
+                    }
+                    foreach (int wrestler in ((CustomSegment)TournamentCard.segment[TournamentCard.CurrentSegment]).rightWresters)
+                    {
+                        AddCharacter(wrestler, 2);
+                    }
+                }
+            }
+        }
+
+
+
+        public static void RemoveCharacter(int id)
+        {
+            Scene_Match_Setup code = UnityEngine.GameObject.Find("Code").GetComponent<Scene_Match_Setup>();
+            Debug.Log("Attempting to remove P" + id.ToString());
+            if (id == 0)
+            {
+                for (int i = 1; i <= NJBJIIIACEP.NBBBLJDBLNM; i++)
+                {
+                    if (NJBJIIIACEP.OAAMGFLINOB[i].AHBNKMMMGFI > 0f)
+                    {
+                        id = i;
+                    }
+                }
+            }
+            if (id > 0)
+            {
+                Debug.Log("Deactivating P" + id.ToString());
+                //    CHLPMKEGJBJ.DNNPEAOCDOG(CHLPMKEGJBJ.AKLKPBJBEBK, 1f, 1f);
+                DFOGOCNBECG dfogocnbecg = NJBJIIIACEP.OAAMGFLINOB[id];
+                dfogocnbecg.AHBNKMMMGFI = -1f;
+                dfogocnbecg.PCNHIIPBNEK[0].SetActive(false);
+                if (dfogocnbecg.IFOCOECLBAF != null)
+                {
+                    dfogocnbecg.IFOCOECLBAF.SetActive(false);
+                }
+                FFCEGMEAIBP.NMMABDGIJNC[id] = -id;
+                if (code.castFoc == id)
+                {
+                    code.castFoc = 0;
+                }
+                if (code.moveFoc == id)
+                {
+                    code.moveFoc = 0;
+                }
+                for (int j = 0; j <= HKJOAJOKOIJ.NGCNKGDDKGF; j++)
+                {
+                    if (HKJOAJOKOIJ.NAADDLFFIHG[j].AHBNKMMMGFI > 0 && HKJOAJOKOIJ.NAADDLFFIHG[j].FOAPDJMIFGP == id)
+                    {
+                        HKJOAJOKOIJ.NAADDLFFIHG[j].FOAPDJMIFGP = 0;
+                    }
+                }
+            }
+        }
+        public static int AddCharacter(int id, int team)
+        {
+            Scene_Match_Setup code = UnityEngine.GameObject.Find("Code").GetComponent<Scene_Match_Setup>();
+            code.n = 0;
+            code.v = 1;
+            while (code.v <= NJBJIIIACEP.NBBBLJDBLNM)
+            {
+                if (NJBJIIIACEP.OAAMGFLINOB[code.v].AHBNKMMMGFI <= 0f)
+                {
+                    Debug.Log("Overwriting slot " + code.v.ToString() + " to add new");
+                    code.n = code.v;
+                    break;
+                }
+                code.v++;
+            }
+            if (code.n == 0 && FFCEGMEAIBP.EHIDHAPMAKG < NAEEIFNFBBO.ILLMCDIFFON && FFCEGMEAIBP.EHIDHAPMAKG < NJBJIIIACEP.KLDJKHPCDHM)
+            {
+                code.n = FFCEGMEAIBP.EHIDHAPMAKG + 1;
+                Debug.Log("Increasing cast to " + FFCEGMEAIBP.EHIDHAPMAKG.ToString() + " to add new");
+            }
+            //    CHLPMKEGJBJ.DNNPEAOCDOG(CHLPMKEGJBJ.DOIEFBGOCPA, 1f, 1f);
+            //   CHLPMKEGJBJ.DNNPEAOCDOG(CHLPMKEGJBJ.JMBMELDCIDA[1], 1f, 0.75f);
+            if (FFCEGMEAIBP.EHIDHAPMAKG < 1)
+            {
+                FFCEGMEAIBP.EHIDHAPMAKG = 1;
+                Debug.Log("Increasing castSize from 0 to 1");
+            }
+            if (code.n > FFCEGMEAIBP.EHIDHAPMAKG)
+            {
+                FFCEGMEAIBP.EHIDHAPMAKG = code.n;
+                Debug.Log("Increasing castSize to " + code.n.ToString());
+            }
+            int num = id;
+            int num2 = 1;
+            int num3 = team;
+            FFCEGMEAIBP.NMMABDGIJNC[code.n] = num;
+            FFCEGMEAIBP.DJCDPNPLICD[code.n] = num2;
+            FFCEGMEAIBP.EKKIPMFPMEE[code.n] = num3;
+            int num4 = 0;
+            int num5;
+            do
+            {
+                FFCEGMEAIBP.AJMAFHIBCGJ[code.n] = (float)UnityEngine.Random.Range(-20, 20) * World.ringSize;
+                if (num3 == 1)
+                {
+                    FFCEGMEAIBP.AJMAFHIBCGJ[code.n] = (float)UnityEngine.Random.Range(-20, -5) * World.ringSize;
+                }
+                if (num3 == 2)
+                {
+                    FFCEGMEAIBP.AJMAFHIBCGJ[code.n] = (float)UnityEngine.Random.Range(5, 20) * World.ringSize;
+                }
+                FFCEGMEAIBP.MHHLHMDOFBP[code.n] = (float)UnityEngine.Random.Range(-20, 20) * World.ringSize;
+                num4++;
+                num5 = 1;
+                if (Mathf.Abs(FFCEGMEAIBP.AJMAFHIBCGJ[code.n]) < 5f && FFCEGMEAIBP.MHHLHMDOFBP[code.n] > 0f && NAEEIFNFBBO.BNCCMMLOIML > 0)
+                {
+                    if (num2 != 3)
+                    {
+                        num5 = 0;
+                    }
+                }
+                else if (num2 == 3)
+                {
+                    num5 = 0;
+                }
+                for (int i = 1; i <= NJBJIIIACEP.NBBBLJDBLNM; i++)
+                {
+                    if (NJBJIIIACEP.OAAMGFLINOB[i].AHBNKMMMGFI > 0f && NAEEIFNFBBO.FHPCDHIGILG(FFCEGMEAIBP.AJMAFHIBCGJ[code.n], FFCEGMEAIBP.MHHLHMDOFBP[code.n], NJBJIIIACEP.OAAMGFLINOB[i].NJDGEELLAKG, NJBJIIIACEP.OAAMGFLINOB[i].BMFDFFLPBOJ) < 5f)
+                    {
+                        num5 = 0;
+                    }
+                }
+            }
+            while (num4 < 100 && num5 == 0);
+            Debug.Log("Assigning character " + FFCEGMEAIBP.NMMABDGIJNC[code.n].ToString() + " to slot " + code.n.ToString());
+            if (code.n <= NJBJIIIACEP.NBBBLJDBLNM)
+            {
+                NJBJIIIACEP.IIACAIINEKD(code.n, id, 1);
+            }
+            else
+            {
+                NJBJIIIACEP.DFLLBNMHHIH(id, 1);
+            }
+            return code.n;
+
+        }
+        public static void RemoveOpponents()
+        {
+            for (int i = NJBJIIIACEP.NBBBLJDBLNM; i > 0; i--)
+            {
+                RemoveCharacter(i);
+            }
+        }
+
 
     }
 }
